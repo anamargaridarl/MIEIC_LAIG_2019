72555c03b93d5a42a4cfdd55d6ba9aa475e3645e
João Martins <up201707311@fe.up.pt>
2019-10-01 19:38:36 +0100

    refactored MyComponent  / implemented display function

--- Trabalho_1/MyComponent.js
+++ Trabalho_1/MyComponent.js
@@ -4,74 +4,55 @@
 */
 class MyComponent extends CGFobject {
 
-    constructor(scene) {
+    constructor(scene, children, materials, texture, transformation) {
         super(scene);
-
-        this.textures = [];
-        this.materials = [];
-        this.transformations = [];
-        this.children = [];
-
-        this.textHerd = false;
-        this.matHerd = false;
-
-        this.initBuffers();
-    }
-
-    activateMathHerd() {
-        this.matHerd = true;
-    }
-
-    activateTextHerd() {
-        this.textHerd = true;
-    }
-
-    pushTexture(t) {
-        this.textures.push(t);
-    }
-
-    setTextures(textures) {
-        this.textures = textures;
-    }
-
-    getTextures() {
-        return this.textures;
-    }
-
-    pushTransformation(t) {
-        this.transformations.push(t);
-    }
-
-    setTransformations(transformations) {
-        this.transformations = transformations;
-    }
-
-    getTransformations() {
-        return this.transformations;
-    }
-
-    pushMaterial(m) {
-        this.materials.push(m);
-    }
-
-    setMaterials(materials) {
+        this.children = children;
         this.materials = materials;
+        this.texture = texture;
+        this.transformation = transformation;
+        this.currMatIndex = 0;
+    }
+    
+    display(textureP,materialP) {
+        if(this.transformation) {
+            this.scene.multMatrix(this.transformation);
+        }
+
+        if(this.materials[this.currMatIndex] == "inherit") {
+            if(this.texture == "inherit") {
+                materialP.setTexture(textureP);
+            }
+            else if (this.texture == "none") {
+                materialP.setTexture(null);
+            }
+            else {
+                materialP.setTexture(this.texture.tex_t);
+            }
+            materialP.apply();
+        }
+        else {
+            if(this.texture == "inherit") {
+                this.materials[this.currMatIndex].setTexture(textureP);
+            }
+            else if (this.texture == "none") {
+                this.materials[this.currMatIndex].setTexture(null);
+            }
+            else {
+                this.materials[this.currMatIndex].setTexture(this.texture);
+            }
+            this.materials[this.currMatIndex].apply();
+        }
+
+        let texP = (this.texture == "inherit") ? textureP: this.texture;
+        let matP = (this.materials[this.currMatIndex] == "inherit") ? materialP : this.materials[this.currMatIndex];
+        for( let children of this.children) {
+            //verificar se e primitva ou nao, aplicar length_s e length_t
+            //children.display(texP,matP);
+        }
     }
 
-    getMaterials() {
-        return this.materials;
-    }
-
-    pushChildren(p) {
-        this.children.push(p);
-    }
-
-    setChildren(materials) {
-        this.materials = materials;
-    }
-
-    getChildren() {
-        return this.materials;
+    updateMaterial() {
+        this.currMatIndex = (this.currMatIndex+1) % this.materials.length;
     }
 
 };
