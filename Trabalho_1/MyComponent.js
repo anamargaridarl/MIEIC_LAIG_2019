80d2651c0582523abdc133ff04302c9e5218855d
João Martins <up201707311@fe.up.pt>
2019-10-01 22:54:55 +0100

    finished component display
    Warning: HIGH CONTENT OF SPAGHETT

--- Trabalho_1/MyComponent.js
+++ Trabalho_1/MyComponent.js
@@ -18,41 +18,53 @@ class MyComponent extends CGFobject {
             this.scene.multMatrix(this.transformation);
         }
 
-        if(this.materials[this.currMatIndex] == "inherit") {
-            if(this.texture == "inherit") {
-                materialP.setTexture(textureP);
-            }
-            else if (this.texture == "none") {
-                materialP.setTexture(null);
-            }
-            else {
-                materialP.setTexture(this.texture.tex_t);
-            }
-            materialP.apply();
-        }
-        else {
-            if(this.texture == "inherit") {
-                this.materials[this.currMatIndex].setTexture(textureP);
-            }
-            else if (this.texture == "none") {
-                this.materials[this.currMatIndex].setTexture(null);
-            }
-            else {
-                this.materials[this.currMatIndex].setTexture(this.texture);
-            }
-            this.materials[this.currMatIndex].apply();
-        }
+        if(this.materials[this.currMatIndex] == "inherit") {                                                                         
+            if(this.texture == "inherit") {                                     
+                materialP.setTexture(textureP);                                     
+            }                                     
+            else if (this.texture == "none") {                                     
+                materialP.setTexture(null);                                     
+            }                                     
+            else {                                     
+                materialP.setTexture(this.texture.tex_t);                                     
+            }                                     
+            materialP.apply();                                     
+        }                                     
+        else {                                     
+            if(this.texture == "inherit") {                                     
+                this.materials[this.currMatIndex].setTexture(textureP);                                     
+            }                                     
+            else if (this.texture == "none") {                                     
+                this.materials[this.currMatIndex].setTexture(null);                                     
+            }                                     
+            else {                                     
+                this.materials[this.currMatIndex].setTexture(this.texture.tex_t);                                     
+            }                                     
+            this.materials[this.currMatIndex].apply();                                     
+        }                                     
 
         let texP = (this.texture == "inherit") ? textureP: this.texture;
         let matP = (this.materials[this.currMatIndex] == "inherit") ? materialP : this.materials[this.currMatIndex];
         for( let children of this.children) {
-            //verificar se e primitva ou nao, aplicar length_s e length_t
-            //children.display(texP,matP);
+            //check if is component
+            if(children instanceof MyComponent) {
+                children.display(texP,matP);
+            }
+            else { // or primitive
+                if(children.updateTexCoords != null) { //if has function to update texCoords (eg: Triangle & Rectangle)
+                    children.updateTexCoords(this.texture.s,this.texture.t);
+                }
+                children.display();
+            }
         }
     }
 
     updateMaterial() {
         this.currMatIndex = (this.currMatIndex+1) % this.materials.length;
+
+        for(let child of this.children) {
+            child.updateMaterial();
+        }
     }
 
 };
