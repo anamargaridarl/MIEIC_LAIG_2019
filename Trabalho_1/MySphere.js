a98392203d3acc776c20879806af5eafbf3c43a5
João Martins <up201707311@fe.up.pt>
2019-10-02 22:51:10 +0100

    fixing sphere

--- Trabalho_1/MySphere.js
+++ Trabalho_1/MySphere.js
@@ -18,28 +18,28 @@ class MySphere extends CGFobject {
         this.texCoords = [];
 
         var phi = 2*Math.PI / this.slices;
-        var teta = 2*Math.PI /this.stacks;
+        var teta = Math.PI /(2*this.stacks);
 
-        for (var st = 0; st <= this.stacks; st++) {
+        for (var st = 0; st <= 2*this.stacks; st++) {
             for (var sl = 0; sl <= this.slices; sl++) {
 
-                var Xaux = Math.cos(phi * sl) * Math.cos(teta * st);
-                var Yaux = Math.sin(phi * sl) * Math.cos(teta * st);
-                var Zaux = Math.sin(teta * st);
+                var Xaux = Math.cos(phi * sl) * Math.sin(teta * st);
+                var Yaux = Math.sin(phi * sl) * Math.sin(teta * st);
+                var Zaux = -Math.cos(teta * st);
 
                 this.vertices.push(this.radius * Xaux, this.radius * Yaux, this.radius * Zaux);
                 this.normals.push(Xaux, Yaux, Zaux);
-
-                this.texCoords.push((this.radius*Xaux+this.radius)/(2*this.radius), 
-                                    1-(this.radius*Yaux+this.radius)/(2*this.radius));
-
+                
+                this.texCoords.push((phi*sl)/(2*Math.PI),
+                1-(teta*st)/(Math.PI));
+              
             }
         }
 
-        for (var st = 0; st < this.stacks; st++) {
+        for (var st = 0; st < 2*this.stacks; st++) {
             for (var sl = 0; sl < this.slices; sl++) {
-                this.indices.push(1 + sl + (this.slices+1) * st, sl + (this.slices +1) * st, sl + (this.slices +1) * (st + 1));
-                this.indices.push(1 + sl + (this.slices +1) * st, sl + (this.slices +1) * (st + 1), 1 + sl + (this.slices +1) * (st + 1));
+                this.indices.push((this.slices+1) * st + sl, (this.slices +1) * st + sl + 1,(this.slices +1) * (st+1) + sl);
+                this.indices.push((this.slices +1) * st + sl + 1,(this.slices +1) * (st + 1) + sl +1,(this.slices +1) * (st + 1) + sl);
 
             }
         }
