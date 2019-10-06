4b177cd533470c75b0a79f179d0634ec2a57fbd1
João Martins <up201707311@fe.up.pt>
2019-10-06 20:07:42 +0100

    CHANGE CAMERA BUT IT LOOKS GOOD
    also updated submission file for errors

--- Trabalho_1/XMLscene.js
+++ Trabalho_1/XMLscene.js
@@ -40,7 +40,7 @@ class XMLscene extends CGFscene {
      * Initializes the scene cameras.
      */
     initCameras() {
-        this.camera = new CGFcamera(0.5, 0.1, 500, vec3.fromValues(10, 50, 50), vec3.fromValues(10, 0, 5));
+        this.camera = new CGFcamera(0.78, 0.1, 500, vec3.fromValues(10, 8, 30), vec3.fromValues(10, 0, 5));
     }
 
   /*  initViews()
