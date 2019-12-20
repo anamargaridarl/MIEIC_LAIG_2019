ebfe4de9247766b40cf61219047606fc28443c87
Ana Margarida <anamargaridarl@gmail.com>
2019-12-20 12:13:35 +0000

    board

--- Trabalho_3/MyComponent.js
+++ Trabalho_3/MyComponent.js
@@ -1,10 +1,10 @@
 /**
-* MyComponent
-* @constructor
-*/
+ * MyComponent
+ * @constructor
+ */
 class MyComponent {
 
-    constructor(scene, children, materials, texture, transformation,animation) {
+    constructor(scene, children, materials, texture, transformation, animation) {
         this.scene = scene;
         this.original = scene;
         this.children = children;
@@ -13,57 +13,52 @@ class MyComponent {
         this.transformation = transformation;
         this.currMatIndex = 0;
         this.animation = animation;
+
+
     }
-    
-    display(textureP,materialP) {
-        
-        if(this.animation != null)
-        {
+
+    display(textureP, materialP) {
+
+        if (this.animation != null) {
             this.scene = this.original;
             this.animation.display(this.scene);
         }
 
-        if(this.transformation) {
+        if (this.transformation) {
             this.scene.multMatrix(this.transformation);
         }
 
-        if(this.materials[this.currMatIndex] == "inherit") {                                                                         
-            if(this.texture.tex_t == "inherit") {                                     
-                materialP.setTexture(this.getParentTexture(textureP));                                     
-            }                                     
-            else if (this.texture.tex_t == "none") {                                     
-                materialP.setTexture(null);                                     
-            }                                     
-            else {                                     
-                materialP.setTexture(this.texture.tex_t);                                     
-            }                                     
-            materialP.apply();                                     
-        }                                     
-        else {                                     
-            if(this.texture.tex_t == "inherit") {                                     
-                this.materials[this.currMatIndex].setTexture(this.getParentTexture(textureP));                                     
-            }                                     
-            else if (this.texture.tex_t == "none") {                                     
-                this.materials[this.currMatIndex].setTexture(null);                                     
-            }                                     
-            else {                                     
-                this.materials[this.currMatIndex].setTexture(this.texture.tex_t);                                     
-            } 
-            this.materials[this.currMatIndex].apply();                                     
-        }                                     
-
-        let texP = (this.texture.tex_t == "inherit") ? textureP: this.texture;
-        let matP = (this.materials[this.currMatIndex] == "inherit") ? materialP : this.materials[this.currMatIndex];
-        for( let children of this.children) {
-            //check if is component
-            if(children instanceof MyComponent) {
-                this.scene.pushMatrix();
-                children.display(texP,matP);
-                this.scene.popMatrix();
+        if (this.materials[this.currMatIndex] == "inherit") {
+            if (this.texture.tex_t == "inherit") {
+                materialP.setTexture(this.getParentTexture(textureP));
+            } else if (this.texture.tex_t == "none") {
+                materialP.setTexture(null);
+            } else {
+                materialP.setTexture(this.texture.tex_t);
             }
-            else { // or primitive
-                if(children.updateTexCoords != null) { //if has function to update texCoords (eg: Triangle & Rectangle)
-                    children.updateTexCoords(texP.s,texP.t);
+            materialP.apply();
+        } else {
+            if (this.texture.tex_t == "inherit") {
+                this.materials[this.currMatIndex].setTexture(this.getParentTexture(textureP));
+            } else if (this.texture.tex_t == "none") {
+                this.materials[this.currMatIndex].setTexture(null);
+            } else {
+                this.materials[this.currMatIndex].setTexture(this.texture.tex_t);
+            }
+            this.materials[this.currMatIndex].apply();
+        }
+
+        let texP = (this.texture.tex_t == "inherit") ? textureP : this.texture;
+        let matP = (this.materials[this.currMatIndex] == "inherit") ? materialP : this.materials[this.currMatIndex];
+        for (let children of this.children) {
+            //check if is component
+            if (children instanceof MyComponent) {
+                this.scene.pushMatrix();
+                children.display(texP, matP);
+                this.scene.popMatrix();
+            } else { // or primitive
+                if (children.updateTexCoords != null) { //if has function to update texCoords (eg: Triangle & Rectangle)
+                    children.updateTexCoords(texP.s, texP.t);
                 }
                 children.display();
             }
@@ -72,29 +67,28 @@ class MyComponent {
     }
 
     getParentTexture(texStruct) {
-        if(texStruct.tex_t == "none")
+        if (texStruct.tex_t == "none")
             return null;
         else
             return texStruct.tex_t;
     }
 
     updateMaterial() {
-        this.currMatIndex = (this.currMatIndex+1) % this.materials.length;
+        this.currMatIndex = (this.currMatIndex + 1) % this.materials.length;
 
-        for(let child of this.children) {
-            if(child instanceof MyComponent)
-            child.updateMaterial();
+        for (let child of this.children) {
+            if (child instanceof MyComponent)
+                child.updateMaterial();
         }
     }
 
-    updateAnimation(t)
-    {
-        if(this.animation)
-        this.animation.update(t,this.scene);
+    updateAnimation(t) {
+        if (this.animation)
+            this.animation.update(t, this.scene);
 
-        for(let child of this.children) {
-            if(child instanceof MyComponent)
-            child.updateAnimation(t,this.scene);
+        for (let child of this.children) {
+            if (child instanceof MyComponent)
+                child.updateAnimation(t, this.scene);
         }
     }
 
--- Trabalho_3/MyGameBoard.js
+++ Trabalho_3/MyGameBoard.js
@@ -2,6 +2,9 @@
  * MyCylinder
  * @constructor
  */
+
+var DEGREE_TO_RAD = Math.PI / 180;
+
 class MyGameBoard extends CGFobject {
 
     constructor(scene, pieces) {
@@ -15,6 +18,24 @@ class MyGameBoard extends CGFobject {
 
     getPiece(row, column) {
         return this.pieces[row][column];
+    }
+
+    applyTransformation(row, col, piece) {
+        this.scene.pushMatrix();
+        let transfMatrix = mat4.create();
+        mat4.translate(transfMatrix, transfMatrix, [row, 0, col]);
+        this.scene.multMatrix(transfMatrix);
+        piece.display();
+        this.scene.popMatrix();
+    }
+
+    display() {
+
+        for (let row = 0; row < 10; row++) {
+            for (let col = 0; col < 10; col++) {
+                this.applyTransformation(row, col, this.pieces[row][col]);
+            }
+        }
     }
 
 }
