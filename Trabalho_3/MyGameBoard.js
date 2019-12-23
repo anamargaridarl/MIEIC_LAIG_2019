62859d63056222695e41212f9a625320db37a9c5
Ana Margarida <anamargaridarl@gmail.com>
2019-12-23 13:39:29 +0000

    board representation finished

--- Trabalho_3/MyGameBoard.js
+++ Trabalho_3/MyGameBoard.js
@@ -46,18 +46,99 @@ class MyGameBoard extends CGFobject {
         }
     }
 
-    applyTransformationRectangle(row, col, unit) {
+    applyTransformationRectangle1(unit) {
         this.scene.pushMatrix();
+        let transfMatrix = mat4.create();
+        mat4.rotateY(transfMatrix, transfMatrix, 90 * DEGREE_TO_RAD);
+        mat4.translate(transfMatrix, transfMatrix, [-4, 0, 1]);
+        this.scene.multMatrix(transfMatrix);
+        unit.piece.display();
+        this.scene.popMatrix();
+    }
+
+    applyTransformationRectangle2(unit) {
+        this.scene.pushMatrix();
+        let transfMatrix = mat4.create();
+        mat4.translate(transfMatrix, transfMatrix, [1, 0, 0]);
+        this.scene.multMatrix(transfMatrix);
+        unit.piece.display();
+        this.scene.popMatrix();
+    }
+
+    applyTransformationRectangle3(unit) {
+        this.scene.pushMatrix();
+        let transfMatrix = mat4.create();
+        mat4.translate(transfMatrix, transfMatrix, [5, 0, 0]);
+        this.scene.multMatrix(transfMatrix);
+        unit.piece.display();
+        this.scene.popMatrix();
+    }
+
+    applyTransformationRectangle4(unit) {
+        this.scene.pushMatrix();
+        let transfMatrix = mat4.create();
+        mat4.rotateY(transfMatrix, transfMatrix, 90 * DEGREE_TO_RAD);
+        mat4.translate(transfMatrix, transfMatrix, [-4, 0, 10]);
+        this.scene.multMatrix(transfMatrix);
+        unit.piece.display();
+        this.scene.popMatrix();
+    }
+
+    applyTransformationRectangle5(unit) {
+        this.scene.pushMatrix();
+        let transfMatrix = mat4.create();
+        mat4.rotateY(transfMatrix, transfMatrix, 90 * DEGREE_TO_RAD);
+        mat4.translate(transfMatrix, transfMatrix, [-9, 0, 10]);
+        this.scene.multMatrix(transfMatrix);
+        unit.piece.display();
+        this.scene.popMatrix();
+    }
+
+    applyTransformationRectangle6(unit) {
+        this.scene.pushMatrix();
+        let transfMatrix = mat4.create();
+        mat4.translate(transfMatrix, transfMatrix, [5, 0, 9]);
+        this.scene.multMatrix(transfMatrix);
+        unit.piece.display();
+        this.scene.popMatrix();
+    }
+
+    applyTransformationRectangle7(unit) {
+        this.scene.pushMatrix();
+        let transfMatrix = mat4.create();
+        mat4.translate(transfMatrix, transfMatrix, [0, 0, 9]);
+        this.scene.multMatrix(transfMatrix);
+        unit.piece.display();
+        this.scene.popMatrix();
+    }
+
+    applyTransformationRectangle8(unit) {
+        this.scene.pushMatrix();
+        let transfMatrix = mat4.create();
+        mat4.rotateY(transfMatrix, transfMatrix, 90 * DEGREE_TO_RAD);
+        mat4.translate(transfMatrix, transfMatrix, [-8, 0, 1]);
+        this.scene.multMatrix(transfMatrix);
         unit.piece.display();
         this.scene.popMatrix();
     }
 
     displayRectangles(pieces) {
-        this.applyTransformationRectangle(0, 0, pieces[0][0]);
-        this.applyTransformationRectangle(1, 0, pieces[1][0]);
+        this.applyTransformationRectangle1(pieces[0][0]);
+        this.applyTransformationRectangle2(pieces[0][1]);
+        this.applyTransformationRectangle3(pieces[0][6]);
+        this.applyTransformationRectangle4(pieces[1][9]);
+        this.applyTransformationRectangle5(pieces[6][9]);
+        this.applyTransformationRectangle6(pieces[9][8]);
+        this.applyTransformationRectangle7(pieces[9][0]);
+        this.applyTransformationRectangle8(pieces[6][0]);
     }
 
     display() {
+
+        this.scene.pushMatrix();
+        let transfMatrix = mat4.create();
+        transfMatrix = mat4.scale(transfMatrix, transfMatrix, [2, 2, 2]);
+        this.scene.multMatrix(transfMatrix);
 
         this.displayRectangles(this.pieces);
         for (let row = 1; row < 9; row++) {
@@ -65,6 +146,9 @@ class MyGameBoard extends CGFobject {
                 this.applyTransformation(row, col, this.pieces[row][col]);
             }
         }
+
+        this.scene.popMatrix();
+
 
     }
 
