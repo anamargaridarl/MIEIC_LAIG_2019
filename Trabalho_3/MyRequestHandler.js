087604f507e8d93bc2cdcbb3c7ae2f7bb9ce2fd3
João Martins <up201707311@fe.up.pt>
2019-12-26 20:13:06 +0000

    finished js side server requests

--- Trabalho_3/MyRequestHandler.js
+++ Trabalho_3/MyRequestHandler.js
@@ -7,6 +7,69 @@ class MyRequestHandler {
     return data;
   }
 
+  static async getPossiblePlays(board,played) {
+    const json = {
+      board : board,
+      plays : played
+    }
+
+    const response = await fetch(`${HOSTPORT}/plays`, {
+      method: 'POST',
+      mode: 'cors', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(json)
+    });
+
+    const data = await response.json();
+    return data;
+  }
+
+  static async playerMove(board,played,player,row,col,t) {
+    const json = {
+      board: board,
+      played: played,
+      player: player,
+      row: row,
+      col: col,
+      t: t
+    }
+
+    const response = await fetch(`${HOSTPORT}/playermove`, {
+      method: 'POST',
+      mode: 'cors', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(json)
+    });
+
+    const data = await response.json();
+    return data;
+  }
+
+  static async cpuMove(board,played,player,level) {
+    const json = {
+      board: board,
+      played: played,
+      player: player,
+      level: level
+    }
+
+    const response = await fetch(`${HOSTPORT}/cpumove`, {
+      method: 'POST',
+      mode: 'cors', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(json)
+    });
+
+    const data = await response.json();
+    return data;
+  }
+
 
 
 
--- Trabalho_3/prolog/server.pl
+++ Trabalho_3/prolog/server.pl
@@ -18,7 +18,7 @@
     boardState(board:list(list), plays:list).
 
 :- json_object
-    possPlays(p:list).
+    possPlays(poss:list).
 
 :- json_object
     cpu_move_preconds(board:list,played:list,player:integer,level:integer).
@@ -29,38 +29,56 @@
 :- json_object
     move_result(played:list,board:list,state:integer).
 
-
 buildBoard(_) :-
-  cors_enable,  
   buildBlankList(B),
   prolog_to_json(board(B), JSONOut),
+  cors_enable,  
   reply_json(JSONOut).
 
+buildBoard(Request) :-
+  option(method(options), Request), !,
+  cors_enable(Request,[methods([get,post,delete])]),
+  format('~n').
+
 getPossiblePlays(Request) :-
-  cors_enable,
   http_read_json(Request, JSONIn),
   json_to_prolog(JSONIn, boardState(B,P)),
   valid_moves(B,P,Out),
-  prolog_to_json(Out,JSONOut),
+  prolog_to_json(possPlays(Out),JSONOut),
+  cors_enable,
   reply_json(JSONOut).
 
+getPossiblePlays(Request) :-
+  option(method(options), Request), !,
+  cors_enable(Request,[methods([get,post,delete])]),
+  format('~n').
+
 playerMove(Request) :-
-  cors_enable,
   http_read_json(Request,JSONIn),
   json_to_prolog(JSONIn, player_move_preconds(Board, Played,Player,Row,Col,T)),
   player_move(Board,Played,Player,Row,Col,T,BoardOut,PlayedOut,StateOut),
   prolog_to_json(move_result(PlayedOut,BoardOut,StateOut),JSONOut),
+  cors_enable,
   reply_json(JSONOut).
 
+playerMove(Request) :-
+  option(method(options), Request), !,
+  cors_enable(Request,[methods([get,post,delete])]),
+  format('~n').
+
 cpuMove(Request) :-
-  cors_enable,
   http_read_json(Request,JSONIn),
   json_to_prolog(JSONIn,cpu_move_preconds(Board,Played,Player,Level)),
   valid_moves(Board,Played,Poss),
   cpu_move(Board,Played,Player,Poss,Level,BoardOut,PlayedOut,StateOut),
   prolog_to_json(move_result(PlayedOut,BoardOut,StateOut),JSONOut),
+  cors_enable,
   reply_json(JSONOut).
   
+cpuMove(Request) :-
+  option(method(options), Request), !,
+  cors_enable(Request,[methods([get,post,delete])]),
+  format('~n').
 
 server :-
   http_server(http_dispatch,[port(8081)]).
