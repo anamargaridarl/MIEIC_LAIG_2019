9759d0e871f76345be49d538f605ae5fa4310b3f
João Martins <up201707311@fe.up.pt>
2020-01-04 17:46:49 +0000

    re added pov changing with turn // removed unnecesary code // fixed request

--- Trabalho_3/MyRequestHandler.js
+++ Trabalho_3/MyRequestHandler.js
@@ -2,7 +2,10 @@ const HOSTPORT = "http://localhost:8081"
 
 class MyRequestHandler {
   static async initBoard() {
-    const response = await fetch(`${HOSTPORT}/create`);
+    const response = await fetch(`${HOSTPORT}/create`, {
+      method: 'POST',
+      mode: 'cors' 
+    });
     const data = await response.json();
     return data;
   }
